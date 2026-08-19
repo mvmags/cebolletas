ee1ae4fdc563ae42266287a818e3754b7821c1bf
mvmags <marco.velasco@gmail.com>
2026-08-19 12:20:05 -0600

    Remove unrelated files from gallery update

--- copal/config/development.js
+++
@@ -1,5 +0,0 @@
-export default Object.freeze({
-  environment: "development",
-  supabaseUrl: "https://unzrsxnosudpvrkzxqbs.supabase.co",
-  supabasePublishableKey: "sb_publishable_K1ZhOjiQ84p0aAxgbUjxcQ_dpXsZC2u"
-});
